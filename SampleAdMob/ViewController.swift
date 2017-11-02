716efc546a0cd6e47eaa922bc1c0689686b60388
Futurismworks <futurismworks@FuturismworksnoMac-mini.local>
2017-11-02 16:41:38 +0900

    Initial Commit

---
+++ SampleAdMob/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleAdMob
+//
+//  Created by Futurismworks on 2017/11/02.
+//  Copyright © 2017年 Futurismworks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
